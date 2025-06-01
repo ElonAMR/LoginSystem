462029d58d4123c274545c733a265e6738644409
Elon Amram <elonamrammm1234@gmail.com>
2025-06-01 17:40:55 +0300

    Installed Library , Create Two Page For Home And Login , And Added Routers

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-//  npm install : express ,dotenv ,mysql2, ejs
+//  npm install : express ,dotenv ,mysql2, ejs ,body-parser
+
+const port = process.env.PORT || 5000;
+const express = require('express');
+const app= express();
+app.use(express.json());
+
+const bodyParser=require('body-parser');
+const path =require('path');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.set("view engine","ejs");
+app.set("views" , path.join(__dirname,"./views"));
+
+
+
+const Login_R = require('./Routers/Login_R');
+app.use('/',Login_R);
+
+app.get( '/' , (req,res) =>{
+    res.render( "homePage" , {} );
+});
+
+app.listen(port , () => {
+    console.log(`Now Listening on port http://localhost:${port}`);
+})
